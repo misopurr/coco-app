30ae1c4a53fd9c4d914e9b7466217066773bc7ea
BiggerRain <15911122312@163.COM>
2024-11-29 22:55:12 +0800

    fix: main.rs & lib.rs (#32)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,42 +1,7 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-use tauri::{AppHandle, Manager, WebviewWindow};
-use tauri_nspanel::{panel_delegate, ManagerExt, WebviewWindowExt};
-
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn change_window_height(handle: AppHandle, height: u32) {
-    let window: WebviewWindow = handle.get_webview_window("main").unwrap();
-
-    let mut size = window.outer_size().unwrap();
-    size.height = height;
-    window.set_size(size).unwrap();
-}
-
-#[tauri::command]
-fn show_panel(handle: AppHandle) {
-    let panel = handle.get_webview_panel("main").unwrap();
-
-    panel.show();
-}
-
-#[tauri::command]
-fn hide_panel(handle: AppHandle) {
-    let panel = handle.get_webview_panel("main").unwrap();
-
-    panel.order_out(None);
-}
-
-#[tauri::command]
-fn close_panel(handle: AppHandle) {
-    let panel = handle.get_webview_panel("main").unwrap();
-
-    panel.released_when_closed(true);
-
-    panel.close();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -44,48 +9,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_nspanel::init())
-        .invoke_handler(tauri::generate_handler![
-            greet,
-            change_window_height,
-            show_panel,
-            hide_panel,
-            close_panel,
-        ])
-        .setup(|app| {
-            init(app.app_handle());
-
-            Ok(())
-        })
+        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
-}
-
-fn init(app_handle: &AppHandle) {
-    let window: WebviewWindow = app_handle.get_webview_window("main").unwrap();
-
-    let panel = window.to_panel().unwrap();
-
-    let delegate = panel_delegate!(MyPanelDelegate {
-        window_did_become_key,
-        window_did_resign_key
-    });
-
-    let handle = app_handle.to_owned();
-
-    delegate.set_listener(Box::new(move |delegate_name: String| {
-        match delegate_name.as_str() {
-            "window_did_become_key" => {
-                let app_name = handle.package_info().name.to_owned();
-
-                println!("[info]: {:?} panel becomes key window!", app_name);
-            }
-            "window_did_resign_key" => {
-                println!("[info]: panel resigned from key window!");
-            }
-            _ => (),
-        }
-    }));
-
-    panel.set_delegate(delegate);
 }
