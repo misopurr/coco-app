ca9adb515bdae86541c8219ec1f111d295e9bb20
ayangweb <75017711+ayangweb@users.noreply.github.com>
2025-02-21 16:52:52 +0800

    chore: support for github action releases (#169)
    * chore: support for github action releases (#165)
    
    * chore: support for generating update files (#168)
    
    * chore: support for github action releases
    
    * chore: support for generating update files
    
    * chore: include TAURI_SIGNING_PRIVATE_KEY_PASSWORD variable in the release
    
    * chore: replacing the pubkey when updating

---
+++ .release-it.ts
@@ -0,0 +1,14 @@
+import type { Config } from "release-it";
+
+export default {
+  git: {
+    commitMessage: "v${version}",
+    tagName: "v${version}",
+  },
+  npm: {
+    publish: false,
+  },
+  hooks: {
+    "after:bump": "tsx scripts/release.ts",
+  },
+} satisfies Config;
