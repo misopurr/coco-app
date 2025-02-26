c18503cb70129a474bb080c1f533f04edfb1bc20
Medcl <m@medcl.net>
2025-02-26 20:12:35 +0800

    chore: add payload to get response (#214)

--- src-tauri/src/common/http.rs
+++ src-tauri/src/common/http.rs
@@ -1,10 +1,12 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct GetResponse {
     pub _id: String,
     pub _source: Source,
     pub result: String,
+    pub payload: Option<Value>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
