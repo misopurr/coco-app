24a915fb31d1ee025de28e65ac2356693d4feca9
Medcl <m@medcl.net>
2025-03-03 21:44:22 +0800

    chore: update the response of new chat (#231)

--- src-tauri/src/common/assistant.rs
+++ src-tauri/src/common/assistant.rs
@@ -1,6 +1,25 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct ChatRequestMessage {
     pub message: Option<String>,
+}
+
+pub struct NewChatResponse {
+    pub _id: String,
+    pub _source: Source,
+    pub result: String,
+    pub payload: Option<Value>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Source {
+    pub id: String,
+    pub created: String,
+    pub updated: String,
+    pub status: String,
+    pub title: Option<String>,
+    pub summary: Option<String>,
+    pub manually_renamed_title: bool,
 }
